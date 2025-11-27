a3146b47fdf50a9ea237a050d27abf8d45452951
MarvinKloe47 <marvin.kloesters@omexom.com>
2025-11-27 14:58:18 +0100

    Hero, About,Skills Implementiert

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../sections/hero/hero.component';
+import { AboutComponent } from '../../sections/about/about.component';
+import { SkillsComponent } from '../../sections/skills/skills.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,12 +1,36 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Skill {
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  skillsTop: Skill[] = [
+    { name: 'Angular', icon: 'assets/icons/angular.png' },
+    { name: 'TypeScript', icon: 'assets/icons/typescript.png' },
+    { name: 'JavaScript', icon: 'assets/icons/javascript.png' },
+    { name: 'HTML5', icon: 'assets/icons/html5.png' },
+    { name: 'CSS3', icon: 'assets/icons/css.png' },
+    { name: 'Restapi', icon: 'assets/icons/restapi.png' },
+    
+    
+  ];
 
+  skillsBottom: Skill[] = [
+    {name: 'Firebase', icon: 'assets/icons/firebase.png' },
+    {name: 'Git', icon: 'assets/icons/git.png' },
+    {name: 'Scrum', icon: 'assets/icons/scrum.png' },
+    {name: 'Materialdesign', icon: 'assets/icons/materialdesign.png' },
+    {name: 'challenge', icon: 'assets/icons/challenge.png' },
+
+  ];
 }
