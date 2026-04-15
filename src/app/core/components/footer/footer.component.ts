e8e848bc6156d644f9df27864038c732e77a51c4
MarvinKloe47 <marvin.kloesters@omexom.com>
2026-04-15 13:45:55 +0200

    add: privacy policy

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ImprintComponent } from '../../../pages/imprint/imprint.component';
+import { TranslationService } from '../../../services/translation/translation.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, ImprintComponent],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  constructor(public translate: TranslationService) {}
 }
